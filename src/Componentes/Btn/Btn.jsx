68047d55a16dcedc844d1e28e0ba0bdde3187e24
Bartolo Del Rosario Marmolejos <juniordelrosario1022@outlook.com>
2023-06-11 13:11:33 -0400

    Corrigiendo estilos

--- src/Componentes/Btn/Btn.jsx
+++ src/Componentes/Btn/Btn.jsx
@@ -8,17 +8,23 @@ const BtnAdd = styled.button`
 text-align: center;
 background-color: ${colorSecundario};
 display: flex;
-border-radius: 3px;
+border-radius: 5px;
 padding: 10px 20px;
 margin-top: 25px;
 font-weight: 600;
 border: 2px solid white;
 color: whitesmoke;
 text-decoration: none;
+transition: all 0.2s;
+
 cursor: pointer;
 @media (max-width: 500px){
     display: none;
     
+}
+
+:hover{
+    transform: scale(1.2);
 }
 `
 
